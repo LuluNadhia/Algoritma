d2ba32ea7dba53659f05556d28edd548ddc013ed
LuluNadhia <101781256+LuluNadhia@users.noreply.github.com>
2022-04-18 15:34:50 +0700

    Add files via upload

---
+++ Pacman.java
@@ -0,0 +1,11 @@
+import model.KartuATM;
+
+import java.util.ArrayList;
+
+public class Pacman {
+    public static void main(String[] args) {
+        ArrayList<Map> daftarMap = new ArrayList<Map>();
+        for (int index = 0; index < 3; index++) {
+            Map pacman = new pacman();
+    }
+}
